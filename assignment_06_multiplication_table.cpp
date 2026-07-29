22f34f0ba082a75fba8c2fc45371769d2e8a5a19
ababiomersharck-driod <ababiomersharck@gmail.com>
2026-07-29 05:58:29 -0700

    Complete multiplication table assignment

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -56,4 +56,36 @@
 
 #include <iostream>
 using namespace std;
+#include <iomanip>
 
+void printTable(int number) {
+    cout << "Multiplication Table for " << number << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << number << "  x  " << setw(2) << left << i << " =  " << number * i << endl;
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    printTable(number);
+
+    cout << endl;
+
+    int n;
+    cout << "Enter a number N: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return 0;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printTable(num);
+        cout << "---------------------------" << endl;
+    }
+
+    return 0;
+}
