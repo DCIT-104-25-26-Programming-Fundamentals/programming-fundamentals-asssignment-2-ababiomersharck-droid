beb0715828a18fb8ac6554a99d15e87dceb09cd4
ababiomersharck-driod <ababiomersharck@gmail.com>
2026-07-29 04:57:45 -0700

    Complete prime checker assignment

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,28 @@
 
 #include <iostream>
 using namespace std;
+bool isPrime(int number) {
+    if (number < 2) {
+        return false;
+    }
+    for (int divisor = 2; divisor < number; divisor++) {
+        if (number % divisor == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num)) {
+        cout << num << " is a prime number." << endl;
+    } else {
+        cout << num << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
